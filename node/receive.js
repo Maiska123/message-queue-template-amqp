e0baa8020ff8ae266662d2ad412e00dab7280d74
Maiska <42906162+Maiska123@users.noreply.github.com>
2023-03-04 20:44:07 +0200

    better feedback

--- node/receive.js
+++ node/receive.js
@@ -8,28 +8,36 @@ await channel.assertQueue(queue, { durable: false });
 
 channel.consume(queue, (msg) => {
 
-    let fields, properties;
+    let info = "1 Uusi Viesti:",
+        fields = "",
+        properties = "";
+
+    if (msg.fields.deliveryTag > 1) {
+        info = msg.fields.deliveryTag.toString() + " Uutta viestiä: "
+    }
 
     for (const [key, val] of Object.entries(msg.fields)) {
         if (typeof val === 'object' && val !== null) {
             for (const [key, val2] of Object.entries(val)) {
-                fields += '\t' + val2 + '+ \n ';
+                fields += key + '\t' + val2 + ' + \n ';
             }
-        } else {
-            fields += '\t' + val + '+ \n ';
+        } else if (val !== undefined || key !== undefined) {
+            fields += key + '\t' + val + ' + \n ';
         }
+
     }
 
     for (const [key, val] of Object.entries(msg.properties)) {
         if (typeof val === 'object' && val !== null) {
             for (const [key, val2] of Object.entries(val)) {
-                properties += '\t' + val2 + '- \n ';
+                properties += key + '\t' + val2 + ' - \n ';
             }
-        } else {
-            properties += '\t' + val + '- \n ';
+        } else if (val !== undefined || key !== undefined) {
+            properties += key + '\t' + val + ' - \n ';
         }
+
     }
 
-    console.log(`1 Uusi Viesti: ${msg.content} \n ${fields} \n ${properties}`)
+    console.log(`${info} ${msg.content} \n ${fields} \n ${properties}`)
 
 })
